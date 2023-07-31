f7cf676cd8d9e9ca4be5c4f60fc95abd910177c5
jan pratt <janice_k_pratt@progressive.com>
2023-07-31 12:41:47 -0400

    Initial commit.

---
+++ EmployeesHrSolution/EmployeesHrAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesHrAPI.Controllers;
+
+public class EmployeesController : ControllerBase
+{
+    //GET /employees
+    [HttpGet("/employees")]
+    public async Task<ActionResult> GetEmployeesAsync()
+    {
+        return Ok("Hello World");
+    }
+
+    //GET /departments
+    [HttpGet("/departments")]
+    public async Task<ActionResult> GetDepartmentsAsync()
+    {
+        var departments = new List<string> { "DEV", "HR", "SALES", "QA" };
+        return Ok(departments);
+    }
+
+}
---
+++ EmployeesHrSolution/EmployeesHrAPI/Program.cs
@@ -0,0 +1,34 @@
+
+namespace EmployeesHrAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger(); //code that lets you get the documentation.
+                app.UseSwaggerUI(); // the middleware that provides the UI for viewing that documentation.
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers(); //When doing controler based apis, this is where it creates the 'lookup'/route table.
+
+            app.Run(); // this is when our API is up and runnig. And it "blocks" here.
+        }
+    }
+}
